0e86c6fcb971cb009bc9c3d44745866ad416654d
trekswithnick <nicholasbrowningdev@outlook.com>
2025-07-15 19:59:57 +1000

    finished numbers

---
+++ starting-out-with-python/chapter7/tuples.py
@@ -0,0 +1,10 @@
+def main():
+    my_tuple = (1, 2, 3, 4, 5)
+    print(my_tuple)
+
+    my_list = list(my_tuple)
+    print(my_list)
+
+    list_tuple = tuple(my_list)
+    print(list_tuple)
+main()
